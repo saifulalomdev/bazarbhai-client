80955fbf0bb6b69999b08b8a5433f9410b413b59
Saiful Alom <saifulalom49590@gmail.com>
2025-06-26 14:04:24 +0600

    i hope this is last commite for bazarbhai client

--- src/pages/OrderList.jsx
+++ src/pages/OrderList.jsx
@@ -17,20 +17,20 @@ export default function OrderList() {
 
   return (
     <div className="max-w-2xl mx-auto p-4">
-      <h1 className="text-xl font-bold">অর্ডার লিস্ট</h1>
+      <div className="flex mb-3 justify-between items-center">
+        <h1 className="text-xl font-bold">অর্ডার লিস্ট</h1>
+        <Paginatore
+          hasMore={data?.hasMore}
+          setPage={setPage}
+          page={page}
+        />
+      </div>
 
       {/* ======== render data ========= */}
       {data?.orders?.length > 0 ? data?.orders?.map(order => (
         <OrderListItem key={order?._id} order={order} />
       )) : <span className="block text-center mt-5">আপনি কোন অর্ডার করেননি। </span>}
 
-      {!isLoading &&
-        data?.orders?.length > 0 &&
-        <Paginatore
-          hasMore={data?.hasMore}
-          setPage={setPage}
-          page={page}
-        />}
     </div>
   );
 }
