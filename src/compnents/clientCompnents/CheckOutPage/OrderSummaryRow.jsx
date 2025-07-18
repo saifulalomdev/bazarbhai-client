49ae154c46517523246b6096cdc9cfb8f47689fa
Saiful Alom <saif.zsdb@gmail.com>
2025-07-18 13:00:07 +0600

    bug fiexd

--- src/compnents/clientCompnents/CheckOutPage/OrderSummaryRow.jsx
+++ src/compnents/clientCompnents/CheckOutPage/OrderSummaryRow.jsx
@@ -1,11 +1,14 @@
 
 export default function OrderSummaryRow({ text, value, className = "border-b" }) {
   return (
-    <div className={`flex justify-between items-center border-b border-gray-300 ${className}`}>
-      <span>{text}</span>
-      <span className="font-bold">
-        {value}
-      </span>
-    </div>
+    <>
+      {value &&
+        <div className={`flex justify-between items-center border-b border-gray-300 ${className}`}>
+          <span>{text}</span>
+          <span className="font-bold">
+            {value}
+          </span>
+        </div>}
+    </>
   )
 }
